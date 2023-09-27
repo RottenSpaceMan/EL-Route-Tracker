52da3f9fffe2fa392e92f0db5f95761f4b700c75
RottenSpaceMan <65809047+RottenSpaceMan@users.noreply.github.com>
2023-09-27 11:33:03 +0530

    second edit

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True, host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'key.pem'))
